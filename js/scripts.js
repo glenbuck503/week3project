3fc99d5e90e75f3feb8a1ac2723779623c4b0c25
Glen Buck <Glen@GlenBuck.local>
2017-12-17 14:46:51 -0800

    add functions. Also I have forgot to commit plenty of times

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,35 @@
 
+  var result = [];
+// function boop(number) {
+//   if
+// }
+var inputtedNumber = parseInt($("input#userInputNumber").val());
+console.log(inputtedNumber);
 
-
-
-
+function test(number) {
+  console.log(number);
+}
 
 $(document).ready(function() {
   $("form#inputnumbers").submit(function(event) {
     event.preventDefault();
     var inputtedNumber = parseInt($("input#userInputNumber").val());
+    test(a)
+    console.log(inputtedNumber);
+    for (var i = 0; i <= inputtedNumber; i++) {
+      if (i == "0") {
+        result.push('Beep!');
+      } else if (i == "1") {
+        result.push('Boop!');
+      } else if (i % 3 == 0) {
+        result.push("I am sorry Dave, I can't do that")
+      } else {
+        result.push(i);
+      }
+      $('#result1').append(result[i]);
 
-    $("#result").text(inputtedNumber);
+    }
+  console.log(result);
 
   });
 });
