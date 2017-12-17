c79b1f25fffd3bffc6884badb28c0a9b5ecd72b7
Glen Buck <Glen@GlenBuck.local>
2017-12-17 15:04:39 -0800

    add css

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,29 @@
+//back end logic
 
-  var result = [];
-// function boop(number) {
-//   if
-// }
-var inputtedNumber = parseInt($("input#userInputNumber").val());
-console.log(inputtedNumber);
+var result = [];
 
-function test(number) {
-  console.log(number);
+
+function replace(number) {
+  for (var i = 0; i <= number; i++) {
+    if (i == "0") {
+      result.push('Beep!');
+    } else if (i == "1") {
+      result.push('Boop!');
+    } else if (i % 3 == 0) {
+      result.push("I am sorry Dave, I can't do that")
+    } else {
+      result.push(i);
+    }
+  }
 }
 
 $(document).ready(function() {
   $("form#inputnumbers").submit(function(event) {
     event.preventDefault();
     var inputtedNumber = parseInt($("input#userInputNumber").val());
-    test(a)
-    console.log(inputtedNumber);
-    for (var i = 0; i <= inputtedNumber; i++) {
-      if (i == "0") {
-        result.push('Beep!');
-      } else if (i == "1") {
-        result.push('Boop!');
-      } else if (i % 3 == 0) {
-        result.push("I am sorry Dave, I can't do that")
-      } else {
-        result.push(i);
-      }
-      $('#result1').append(result[i]);
+    replace(inputtedNumber);
+    //console.log(inputtedNumber);
 
-    }
   console.log(result);
 
   });
